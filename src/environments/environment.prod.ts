d5c076d84e820222c5ab383967509a6d89b14676
Boris <boris.stankovv@gmail.com>
2022-07-08 11:29:23 +0300

    asd

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'noteboo-ee2a7',
+    appId: '1:279631409689:web:29aadcaf26499a2abc7088',
+    databaseURL: 'https://noteboo-ee2a7-default-rtdb.firebaseio.com',
+    storageBucket: 'noteboo-ee2a7.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyAFd3gWvdWSrFZdxKgyfdXisU_RpH6PROg',
+    authDomain: 'noteboo-ee2a7.firebaseapp.com',
+    messagingSenderId: '279631409689',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'noteboo-ee2a7',
+    appId: '1:279631409689:web:29aadcaf26499a2abc7088',
+    databaseURL: 'https://noteboo-ee2a7-default-rtdb.firebaseio.com',
+    storageBucket: 'noteboo-ee2a7.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyAFd3gWvdWSrFZdxKgyfdXisU_RpH6PROg',
+    authDomain: 'noteboo-ee2a7.firebaseapp.com',
+    messagingSenderId: '279631409689',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
